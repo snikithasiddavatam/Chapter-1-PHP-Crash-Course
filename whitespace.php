11940c67a75ecf915c9d8e2bbe5495cbd8fa18a6
snikithasiddavatam <39854699+snikithasiddavatam@users.noreply.github.com>
2019-06-23 21:40:39 +0530

    Day 2

---
+++ whitespace.php
@@ -0,0 +1,20 @@
+<?php
+echo 'hello ';
+echo 'world';
+// and
+echo 'hello ';echo 'world';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Whitespace</title>
+</head>
+<body>
+    <h1>Welcome to Bob's Auto Parts!</h1><p>What would you like to order today?</p>
+    <!--and-->
+    <h1>Welcome                to Bob's
+    Auto Parts!</h1>
+    <p>What would you like 
+    to order today?</p>
+</body>
+</html>
